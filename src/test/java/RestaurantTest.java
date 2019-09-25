839272d34f128dc33029bd917c38ac28aa3b7734
Muyenzi Raissa <muyenziraissa@gmail.com>
2019-09-25 17:49:16 +0200

    first commit

---
+++ src/test/java/RestaurantTest.java
@@ -0,0 +1,15 @@
+import org.junit.After;
+import org.junit.Before;
+
+public class RestaurantTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
